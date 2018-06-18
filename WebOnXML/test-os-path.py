92bcf55593d0b95976088add6f5ddae5abcf01cc
David Pay <pc_afoundo@hotmail.com>
2018-06-18 19:19:47 +0800

    some troubles about debug and cmd. fixed

--- WebOnXML/test-os-path.py
+++ WebOnXML/test-os-path.py
@@ -9,3 +9,6 @@ testpath = 'testpath'
 path12 = os.path.join(*[testpath])
 if not os.path.isdir(path12):
     os.makedirs(path12)
+
+#when i run python website.py, it's different in cmd mode and debug mode because the start path is different.
+#so, i change the cwd in lauch.json like "cwd": "${fileDirname}"
